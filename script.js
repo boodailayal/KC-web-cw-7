ac17bee483f2d94dbb290a0d1e72212d856addb5
boodailayal <109864473+boodailayal@users.noreply.github.com>
2022-08-17 23:02:18 +0300

    did some changes

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+function BMI(weight, height){
+  let result = weight / (height * height);
+  return result;
+}
 
+console.log (BMI(70,1.7));
+
+function Status (Bmi) {
+if (BMI < 18.5) {
+
+    return("لديك نقص بالوزن");
+} else if (BMI >= 18.5 && BMI <25){
+    return `وزنك صحي`;
+}
+ 
+
+} else (BMI >= 25){
+
+    return`لديك زياده بالوزن`;
+}
+
+
+
+
+function calculate () {
+
+let weight = document.getElementById(`weight`).value;
+let hight = document.getElementById(`hight`).value;
+
+
+let BMI = (weight,hight);
+let desc = Status(BMI);
+
+let div = document.getElementById('result');
+
+div.innerTEXT = BMI + " == " + desc
+}s
